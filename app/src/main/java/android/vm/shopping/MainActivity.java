98e81397e7cd6ee8d5f68bd283253fb2f24f43ad
Alessio <apinna.elis@gmail.com>
2017-01-25 11:33:49 +0100

    created first acivity

---
+++ app/src/main/java/android/vm/shopping/MainActivity.java
@@ -0,0 +1,16 @@
+package android.vm.shopping;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Alessio-Valeria on 25/01/2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
